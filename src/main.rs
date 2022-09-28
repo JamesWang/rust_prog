dee21bf21fb7d9fdbd23c00b3eda90dc1a9db6cf
DESKTOP-84BFPQT\james <james.wang.w@gmail.com>
2022-09-27 21:57:01 -0400

    upgrade actix_web version to latest, and make code to work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 mod rhttp;
-fn main() {
-    rhttp::rhttp::create_server()
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use rhttp::rhttp;
+    HttpServer::new(|| {
+        App::new()
+            .service(rhttp::get_index)
+            .service(rhttp::post_gcd)
+    })
+        .bind("127.0.0.1:3000")
+        .expect("Bind ")
+        .run()
+        .await
 }
--- src/rhttp/rhttp.rs
+++ src/rhttp/rhttp.rs
@@ -1,6 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
-use actix_web::dev::Server;
+use actix_web::{get, HttpResponse, post, web};
 use serde::Deserialize;
+
 use rust_prog::gcd;
 
 #[derive(Deserialize)]
@@ -10,7 +10,8 @@ pub struct GcdParameters {
 }
 
 
-pub fn get_index() -> HttpResponse {
+#[get("/")]
+pub async fn get_index() -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/html")
         .body(
@@ -25,25 +26,16 @@ pub fn get_index() -> HttpResponse {
         )
 }
 
-pub fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+#[post("/gcd")]
+pub async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
     if form.n == 0 || form.m == 0 {
         return HttpResponse::BadRequest()
             .content_type("text/html")
             .body("Computing the GCD with zero is boring.");
     }
     let response =
-        format!("The greatest common divisor of the numbers {} and {} is <b>{}</b>\n", form.n, form.m, gcd(form.n, form.m));
+        format!("The greatest common divisor of the numbers {} and {} is <b>{}</b>\n",
+                form.n, form.m, gcd(form.n, form.m));
     HttpResponse::Ok().content_type("text/html").body(response)
 }
 
-pub fn create_server() {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(get_index))
-            .route("/gcd", web::post().to(post_gcd))
-    })
-        .bind("127.0.0.1:3000")
-        .expect("Error binding to address")
-        .run()
-        .expect("Error to create server")
-}
