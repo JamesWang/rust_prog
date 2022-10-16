42bb406743a3face9930f04458052841816c0294
DESKTOP-84BFPQT\james <james.wang.w@gmail.com>
2022-10-16 12:37:30 -0400

    max diff with the two valeus produce the diff

--- src/algo/algos.rs
+++ src/algo/algos.rs
@@ -1,23 +1,22 @@
 use std::ops::Sub;
+use num::Num;
 
-pub fn max_diff<T: Ord + Sub<Output=T> + Copy>(ints: &[T], min: T) -> T {
+pub fn max_diff<T: PartialOrd + Sub<Output=T> + Copy>(ints: &[T], min: T) -> T {
     //tail-recursive
-    fn find_max_diff<T: Ord + Sub<Output=T> + Copy>(max: &T, diff: T, rest: &[T]) -> T {
+    fn find_max_diff<T: PartialOrd + Sub<Output=T> + Copy>(max: &T, diff: T, rest: &[T]) -> T {
         if rest.is_empty() {
             return diff;
         }
         if rest[0] > *max {
             if rest.len() > 1 {
-                let m = &rest[0];
-                find_max_diff(m, diff, &rest[1..])
-            } else {
-                diff
-            }
+                find_max_diff(&rest[0], diff, &rest[1..])
+            } else { diff }
         } else {
+            let n_diff = max.sub(rest[0]);
             find_max_diff(
                 max,
-                Ord::max(diff, max.sub(rest[0])),
-                &rest[1..]
+                if PartialOrd::ge(&diff, &n_diff) { diff } else { n_diff },
+                &rest[1..],
             )
         }
     }
@@ -29,10 +28,74 @@ pub fn max_diff<T: Ord + Sub<Output=T> + Copy>(ints: &[T], min: T) -> T {
     find_max_diff(&min, reverse[0], &reverse[1..])
 }
 
-#[test]
-fn test_find_max_diff_i32() {
-    assert_eq!(
-        max_diff(&[7, 1, 5, 3, 6, 4], i32::MIN),
-        5
-    )
+#[cfg(test)]
+mod find_tests {
+    use super::*;
+
+    #[test]
+    fn test_find_max_diff_i32() {
+        assert_eq!(
+            max_diff(&[7, 1, 5, 3, 6, 4], i32::MIN),
+            5
+        );
+    }
+
+    #[test]
+    fn test_find_max_diff_float() {
+        let arr: [f32; 6] = [7.4, 1.2, 5.8, 3.1, 6.0, 4.4];
+        assert_eq!(
+            max_diff(&arr, f32::MIN),
+            4.8
+        )
+    }
+
+    #[test]
+    fn test_find_max_diff_info() {
+        assert_eq!(
+            max_diff_info(&[7, 1, 5, 3, 6, 4], i32::MIN),
+            (5, (1, 6))
+        );
+    }
+
+    #[test]
+    fn test_find_max_diff2() {
+        assert_eq!(
+            max_diff2(&[7, 1, 5, 3, 6, 4], i32::MIN),
+            5
+        );
+    }
+}
+type Pair<T> = (T, T);
+
+pub fn max_diff_info<T: PartialOrd + Sub<Output=T> + Copy>(ints: &[T], min: T) -> (T, Pair<T>) {
+    //tail-recursive
+    fn find_max_diff<T: PartialOrd + Sub<Output=T> + Copy>(max: &T, diff: T, rest: &[T], pair: Pair<T>) -> (T, Pair<T>) {
+        if rest.is_empty() {
+            return (diff, pair);
+        }
+        if rest[0] > *max {
+            if rest.len() > 1 {
+                find_max_diff(&rest[0], diff, &rest[1..], pair)
+            } else { (diff, pair) }
+        } else {
+            let n_diff = max.sub(rest[0]);
+            let c_diff = if PartialOrd::ge(&diff, &n_diff) { diff } else { n_diff };
+            find_max_diff(
+                max,
+                c_diff,
+                &rest[1..],
+                (rest[0], *max)
+            )
+        }
+    }
+    if ints.len() <= 1 {
+        return (min, (min, min));
+    }
+    let mut reverse = ints.to_vec();
+    reverse.reverse();
+    find_max_diff(&min, reverse[0], &reverse[1..], (min, min))
+}
+
+fn max_diff2<T: PartialOrd + Sub<Output=T> + Copy>(ints: &[T], min: T) -> T {
+    return max_diff_info(ints, min).0
 }
--- src/lib.rs
+++ src/lib.rs
@@ -54,19 +54,19 @@ pub fn gcd_runner() {
     println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
-pub struct Queue {
-    older: Vec<char>,
-    younger: Vec<char>,
+pub struct Queue<T> {
+    older: Vec<T>,
+    younger: Vec<T>,
 }
 
-impl Queue {
+impl<T> Queue<T> {
     //associated functions, functions not defined as an impl block's item called free function
 
-    pub fn push(&mut self, c: char) {
+    pub fn push(&mut self, c: T) {
         self.younger.push(c);
     }
 
-    pub fn pop(&mut self) -> Option<char> {
+    pub fn pop(&mut self) -> Option<T> {
         if self.older.is_empty() {
             if self.younger.is_empty() {
                 return None;
@@ -86,15 +86,15 @@ impl Queue {
     }
 }
 
-impl Queue {
-    pub fn split(self) -> (Vec<char>, Vec<char>) {
+impl<T> Queue<T> {
+    pub fn split(self) -> (Vec<T>, Vec<T>) {
         (self.older, self.younger)
     }
 }
 
 #[test]
 fn test_queue() {
-    let mut q = Queue { older: Vec::new(), younger: Vec::new() };
+    let mut q: Queue<char> = Queue { older: Vec::new(), younger: Vec::new() };
     q.push('0');
     q.push('1');
     assert_eq!(q.pop(), Some('0'));
@@ -124,7 +124,64 @@ impl Node {
         }
     }
 
-    fn append_to(self: Rc<self>, parent: &mut Node) {
-        parent.children.push(self))
+    fn append_to(self: Rc<Self>, parent: &mut Node) {
+        parent.children.push(self)
     }
+}
+
+impl<T> Queue<T> {
+    pub fn new() -> Queue<T> {
+        Self { older: Vec::new(), younger: Vec::new() }
+    }
+}
+
+pub struct Vector2 {
+    x: f32,
+    y: f32,
+}
+
+impl Vector2 {
+    const ZERO: Vector2 = Vector2 { x: 0.0, y: 0.0 };
+    const UNIT: Vector2 = Vector2 { x: 1.0, y: 1.0 };
+}
+
+//given any specific lifetime 'elt, you can make Extrema<'elt>
+// that holds references with that lifetime
+struct Extrema<'elt> {
+    greatest: &'elt i32,
+    least: &'elt i32,
+}
+
+
+//it’s so common for the return type to use the same lifetime as an argument
+//Rust lets us omit the lifetimes when there’s one obvious candidate.
+
+//fn find_extrema<'s>(slice: &'s [i32]) -> Extrema<'s> {
+fn find_extrema(slice: &[i32]) -> Extrema {
+    let mut greatest = &slice[0];
+    let mut least = &slice[0];
+
+    for item in slice.iter().skip(1) {
+        if item < least {
+            least = &item;
+        }
+        if item > greatest {
+            greatest = &item;
+        }
+    }
+    Extrema { greatest, least }
+}
+
+#[test]
+fn test_find_extrema() {
+    let a = [0, -3, 0, 15, 48];
+    let e = find_extrema(&a);
+    assert_eq!(*e.least, -3);
+    assert_eq!(*e.greatest, 48);
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct Point {
+    x: f64,
+    y: f64
 }
