1ff2c795df1bc7823f1ec2fc89fe53b48cb712fe
DESKTOP-84BFPQT\james <james.wang.w@gmail.com>
2022-10-15 21:21:42 -0400

    rust struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use std::str::FromStr;
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_gcd() {
         assert_eq!(gcd(14, 15), 1);
@@ -51,4 +52,79 @@ pub fn gcd_runner() {
     }
 
     println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
+
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>,
+}
+
+impl Queue {
+    //associated functions, functions not defined as an impl block's item called free function
+
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+            //younger is not empty, old is empty
+            use std::mem::swap;
+            //bring the elements in younger to older
+            swap(&mut self.older, &mut self.younger);
+            //make it FIFO
+            self.older.reverse();
+        }
+        self.older.pop()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+}
+
+impl Queue {
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
+}
+
+#[test]
+fn test_queue() {
+    let mut q = Queue { older: Vec::new(), younger: Vec::new() };
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.pop(), Some('0'));
+
+    q.push('$');
+    assert_eq!(q.pop(), Some('1'));
+    assert_eq!(q.pop(), Some('$'));
+    assert_eq!(q.pop(), None);
+
+    assert!(q.is_empty());
+    q.push('☉');
+    assert!(!q.is_empty());
+}
+
+use std::rc::Rc;
+
+struct Node {
+    tag: String,
+    children: Vec<Rc<Node>>,
+}
+
+impl Node {
+    fn new(tag: &str) -> Node {
+        Node {
+            tag: tag.to_string(),
+            children: vec![],
+        }
+    }
+
+    fn append_to(self: Rc<self>, parent: &mut Node) {
+        parent.children.push(self))
+    }
 }
